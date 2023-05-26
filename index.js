82200e61862ccb2c4baa15dd79850a8eaa4e32cc
Christopher <qaetre@gmail.com>
2023-05-26 13:44:34 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body:JSON.stringify({name:`${name}`, email:`${email}`})
+    
+    }).then(res => res.json()).then(data => {
+        const displayDom = document.createElement('p')
+        const idToDisplay = data["id"]
+        displayDom.textContent = idToDisplay
+        document.body.append(displayDom)
+    }).catch(data => {
+        const displayDom = document.createElement('p')
+        const idToDisplay = data["message"]
+        displayDom.textContent = idToDisplay
+        document.body.append(displayDom)
+    })
+}
